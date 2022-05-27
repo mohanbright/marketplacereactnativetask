84f8b20ddfea496cc07d7e3e178a3641d2af4c42
Mohan Bright <excellentapp7@gmail.com>
2022-05-27 15:32:45 +0530

    # scrollview view Indicator hide and keyboard handle

--- App.js
+++ App.js
@@ -3,47 +3,58 @@
  * @flow strict-local
  */
 
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import {
   SafeAreaView,
   StatusBar,
   View,
   Dimensions,
   Platform,
-} from 'react-native';
-import colors from './src/constants/colors';
-import {persistor, store} from './src/redux/store';
-import {PersistGate} from 'redux-persist/integration/react';
-import {connect, Provider} from 'react-redux';
-import LoginScreen from './src/screens/Login';
+} from "react-native";
+import colors from "./src/constants/colors";
+import { persistor, store } from "./src/redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { connect, Provider } from "react-redux";
+import LoginScreen from "./src/screens/Login";
 import {
   ChangeDimension,
   ChangeOrientation,
-} from './src/redux/actions/dimensionActions';
+} from "./src/redux/actions/dimensionActions";
 
 let timer;
 let addEventListener;
 
-const screenHeight = Dimensions.get('screen').height;
-const windowHeight = Dimensions.get('window').height;
+const screenHeight = Dimensions.get("screen").height;
+const screenWidth = Dimensions.get("screen").width;
+const windowHeight = Dimensions.get("window").height;
 const differnce = screenHeight - windowHeight;
 
-const navigationContainer = ({dimension, changeOrientation, ...props}) => {
+const navigationContainer = ({ dimension, changeOrientation, ...props }) => {
   const [RealHeight, setRealHeight] = useState(
-    Platform.OS === 'ios'
+    Platform.OS === "ios"
       ? windowHeight
       : differnce > 1
       ? windowHeight
-      : windowHeight - StatusBar.currentHeight,
+      : windowHeight - StatusBar.currentHeight
   );
   useEffect(() => {
     if (addEventListener) addEventListener?.remove();
-    addEventListener = Dimensions.addEventListener('change', ({screen}) => {
-      setRealHeight(screen.height);
+    addEventListener = Dimensions.addEventListener("change", ({ screen }) => {
+      if (screen.height > screen.width) {
+        setRealHeight(
+          Platform.OS === "ios"
+            ? windowHeight
+            : differnce > 1
+            ? windowHeight
+            : windowHeight - StatusBar.currentHeight
+        );
+      } else {
+        setRealHeight(screenWidth);
+      }
     });
     return () => addEventListener.remove();
   }, []);
-  const onLayout = ({nativeEvent: {layout}}) => {
+  const onLayout = ({ nativeEvent: { layout } }) => {
     if (timer) clearTimeout(timer);
     if (layout.y > 0)
       setTimeout(() => {
@@ -53,32 +64,36 @@ const navigationContainer = ({dimension, changeOrientation, ...props}) => {
   return (
     <SafeAreaView
       style={{
-        width: '100%',
+        width: "100%",
         height: RealHeight,
         backgroundColor: colors.white,
-      }}>
+      }}
+    >
       <View
         onLayout={onLayout}
         style={{
           flex: 1,
-        }}>
+        }}
+      >
         <StatusBar backgroundColor={colors.white} />
         <LoginScreen />
       </View>
     </SafeAreaView>
   );
 };
-const mapStatetoProps = state => ({dimension: state.deviceDimensionReducer});
-const mapDispatchtoProps = dispatch => ({
-  changeDimension: layout => dispatch(ChangeDimension(layout)),
+const mapStatetoProps = (state) => ({
+  dimension: state.deviceDimensionReducer,
+});
+const mapDispatchtoProps = (dispatch) => ({
+  changeDimension: (layout) => dispatch(ChangeDimension(layout)),
   changeOrientation: () => dispatch(ChangeOrientation()),
 });
 const NavigationContainer = connect(
   mapStatetoProps,
-  mapDispatchtoProps,
+  mapDispatchtoProps
 )(navigationContainer);
 
-const App = props => {
+const App = (props) => {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
